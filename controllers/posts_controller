3e703ee9a85d8524669dadddb1ce3a57ccb33727
afzalkhan <afzalkhann76@gmail.com>
2023-03-24 05:13:09 +0530

    created more controller and routers

---
+++ controllers/posts_controller
@@ -0,0 +1,3 @@
+module.exports.posts=function(req,res){
+    res.end("<h1> thia posts controller</h1>")
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express=require('express');
-const home_controller = require('../controllers/home_controller');
+
 const router=express.Router()
 const home_cont=require('../controllers/home_controller')
 router.get('/',home_cont.home);
+router.use('/user',require('./user'))
+router.use('/posts',require('./posts'))
 
 module.exports=router;
