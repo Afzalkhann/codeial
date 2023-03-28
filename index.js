5d06ad9702350b74eec3f89aaabc9c5e20d1cc36
afzalkhan <afzalkhann76@gmail.com>
2023-03-28 23:04:06 +0530

    just mongoose remains

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express=require('express')
 const app=express()
 const port=8000
+const expressLayouts=require('express-ejs-layouts')
+
+
+app.use(express.static('./assets'))
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+app.use(expressLayouts)
 //use express router
-app.use("/",require("./routes/index"));
+app.use("/",require("./routes"));
 app.set('view engine','ejs');
-app.use('views','./views');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
