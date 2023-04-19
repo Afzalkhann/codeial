8a0a8e09e91831ae756c9d4fe782da3bb9d102d2
afzalkhan <afzalkhann76@gmail.com>
2023-04-19 23:26:30 +0530

    replaced call back functions with async

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,25 +1,25 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = async function(req, res){
+    try{
+        let post=await Post.findById(req.body.post)
+        let comment=await Comment.create({
+            content: req.body.content,
+            post: req.body.post,
+            user: req.user._id
+            })
 
-        if (post){
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            }, function(err, comment){
-                // handle error
+            post.comments.push(comment);
+            post.save();
 
-                post.comments.push(comment);
-                post.save();
-
-                res.redirect('/');
-            });
-        }
-
-    });
+            res.redirect('/');
+               
+    }
+    catch(err){
+        console.log('error',err)
+    }
+      
 }
 
 module.exports.destroy=function(req,res){
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,23 +1,32 @@
 const User = require('../models/user');
 
 
-module.exports.profile = function(req, res){
-    User.findById(req.params.id,function(err,user){
+module.exports.profile = async function(req, res){
+    try{
+        let user= await User.findById(req.params.id)
         return res.render('user_profile', {
-            title: 'User Profile',
-            profile_user:user
-        })
-    })
+                title: 'User Profile',
+                profile_user:user
+            })
+    }
+    catch(err){
+        console.log('error',err)
+    }
    
 }
-module.exports.update=function(req,res){
-    if(req.user.id==req.params.id){
-        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+module.exports.update= async function(req,res){
+    try{
+        if(req.user.id==req.params.id){
+            let user=await User.findByIdAndUpdate(req.params.id,req.body)
             return res.redirect('back')
-        })
+           
+        }
+        else{
+            return res.status(401).send('Unauthorized')
+        }
     }
-    else{
-        return res.status(401).send('Unauthorized')
+    catch(err){
+        console.log('error',err)
     }
 }
 
@@ -47,27 +56,26 @@ module.exports.signIn = function(req, res){
 }
 
 // get the sign up data
-module.exports.create = function(req, res){
+module.exports.create =async function(req, res){
+    try{
     if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
-
+    let user=await User.findOne({email: req.body.email})
+        
         if (!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
-
-                return res.redirect('/users/sign-in');
-            })
-        }else{
+        let user=await User.create(req.body)
+        return res.redirect('/users/sign-in');
+        }
+        else{
             return res.redirect('back');
         }
-
-    });
+    }
+    catch(err){
+        console.log('errpr')
+    }
 }
-
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
